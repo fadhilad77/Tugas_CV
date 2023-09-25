cdd4610555fd6cbe5bbc069d909435ab65ce9437
Fadhil Daniswara <fadhildaniswara@Guswal-MacBook-Air.local>
2023-09-25 20:06:23 +0700

    first commit

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char input[101];
+
+    fgets(input, sizeof(input), stdin);
+
+    size_t len = strlen(input);
+    if(len > 0 && input[len-1] == '\n') {
+        input[len-1] = '\0';
+    }
+
+    printf("#include <stdio.h>\n");
+    printf("int main()\n");
+    printf("{\n");
+    printf("    printf(\"%%s\\n\", \"%s\");\n", input);
+    printf("    return 0;\n");
+    printf("}\n");
+
+    return 0;
+}
